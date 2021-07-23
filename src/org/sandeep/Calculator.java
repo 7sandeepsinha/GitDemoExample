075979da6b1dcfa8896bb3d822542190b80aca0a
sndpoffcl <sandeep2official@gmail.com>
2021-07-23 21:05:19 +0530

    New project

---
+++ src/org/sandeep/Calculator.java
@@ -0,0 +1,13 @@
+package org.sandeep;
+
+public class Calculator {
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static int sub(int a, int b){
+        return a-b;
+    }
+}
+
